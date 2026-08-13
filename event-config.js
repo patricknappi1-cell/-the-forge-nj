76cd99e190a777996b409cd8cd66b5b644bc46c0
patricknappi1-cell <patrick.nappi1@gmail.com>
2026-08-13 08:09:09 -0400

    Add events calendar and Testimony Night pages

---
+++ event-config.js
@@ -0,0 +1,2 @@
+// Google Apps Script web app for The Forge Testimony Night forms.
+window.FORGE_EVENT_FORM_URL = "https://script.google.com/macros/s/AKfycbzbWJnLxq4K9FznOHIdz6mND5bKbPsXLEvNkXylLJyVN6wLq6r7Of0FC77RsYYogxzLoA/exec";
